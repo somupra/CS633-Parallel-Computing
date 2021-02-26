31dcd3498ade7337e296882885dbb9587cff779b
Somu Prajapati <somupra@iitk.ac.in>
2021-02-26 19:43:42 +0530

    minor fix in makefile

--- run.sh
+++ run.sh
@@ -10,17 +10,15 @@ fi
 
 /users/btech/somupra/UGP/allocator/src/allocator.out 64 8 > dump.txt
 
-for execution in {1..5}
-do
+for execution in {1..2}
+do  
+    i=1
 	for P in 16 36 49 64
     do
-        i=1
 	    for N in $((16*16)) $((32*32)) $((64*64)) $((128*128)) $((256*256)) $((512*512)) $((1024*1024))
         do  
-            # printf "$P $N $1\n"
-
             mpirun -np $P -f hostsimproved ./halo $N $1 >> data$i.txt
         done
-        i++
+        ((i++))
     done
 done
