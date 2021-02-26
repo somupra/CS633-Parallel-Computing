50a61463e32bfb96f37d8858b6187a9071bd9444
Somu Prajapati <somupra@iitk.ac.in>
2021-02-26 17:21:45 +0530

    add script

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+if [ ! -f halo ]; then
+    printf "halo does not exist, execute make first\n"
+    exit 1
+fi
+
+if [ "$#" -ne 1 ]; then
+    printf "USAGE: [NUM TIME STEPS]\n"
+    exit 1
+fi
+
+for execution in {1..5}
+do
+	for P in 16 36 49 64
+    do
+        for N in 16*16 32*32 64*64 128*128 256*256 512*512 1024*1024
+        do  
+            # printf "$P $N $1\n"
+            mpirun -np $P ./halo $N $1 >> data.txt
+        done
+    done
+done
