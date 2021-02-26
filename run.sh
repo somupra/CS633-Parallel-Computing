9e7f8855e7b018bcef28120e453ec45dcf86995a
Somu Prajapati <somupra@cse.iitk.ac.in>
2021-02-26 18:39:59 +0530

    first run on server, data generated

--- run.sh
+++ run.sh
@@ -9,6 +9,8 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+/users/btech/somupra/UGP/allocator/src/allocator.out 64 8 > dump.txt
+
 for execution in {1..5}
 do
 	for P in 16 36 49 64
@@ -16,7 +18,8 @@ do
 	    for N in $((16*16)) $((32*32)) $((64*64)) $((128*128)) $((256*256)) $((512*512)) $((1024*1024))
         do  
             # printf "$P $N $1\n"
-            mpirun -np $P -f hostfile.txt ./halo $N $1 >> data.txt
+
+            mpirun -np $P -f hostsimproved ./halo $N $1 >> data.txt
         done
     done
 done
